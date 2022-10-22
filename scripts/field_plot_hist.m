3a3ecbd9a518fe05921eaeb5066a57331afc9aba
dlr1516 <dario.lodirizzini@unipr.it>
2022-10-22 17:15:22 +0200

    added comments to some of the scripts

--- scripts/field_plot_hist.m
+++ scripts/field_plot_hist.m
@@ -1,4 +1,29 @@
 function field_plot_hist(field,xmin,xmax,ymin,ymax,res)
+% FIELD Plots the field with the received water volume
+%   
+%   field_plot_hist(field,xmin,xmax,ymin,ymax,res)
+%
+% Plots the irrigation outcome of a given field. The size of the plot as well as
+% the output histogram resolution can be customized by the user.  
+%
+% INPUT:
+% - field: the field object to be plotted;
+% - xmin: minimum coordinate x in the histogram;
+% - xmax: maximum coordinate x in the histogram; 
+% - ymin: minimum coordinate y in the histogram;
+% - ymax: maximum coordinate y in the histogram; 
+% - res: resolution of the output histogram. 
+
+% OUTPUT:
+% - a window with the plot of the histogram.
+%
+%
+% VRI Sprinkler Simulation
+% Copyright (C) 2022 Dario Lodi Rizzini, Gabriele Penzotti.
+% 
+% vri-sprinkler-simulation is free software: you can redistribute it and/or modify
+% it under the terms of the Creative Common License.
+%
   S_cell = field.cell_size^2;
   idx_x_min = round(xmin / field.cell_size) + 1
   idx_x_max = round(xmax / field.cell_size) + 1
--- scripts/field_water_volume.m
+++ scripts/field_water_volume.m
@@ -1,4 +1,23 @@
 function [volume_tot,height_avg] = field_water_volume(field)
+% FIELD Returns the water volumes and average heights
+%   
+%   field_plot(field,type)
+%
+% Returns the water volumes and average heights for each cell used in simulation.
+%
+% INPUT:
+% - field: the field object.
+% 
+% OUTPUT:
+% - volume_tot: total volume of water fallen in the field;
+% - height_avg: average water height fallen in the field.
+%
+% VRI Sprinkler Simulation
+% Copyright (C) 2022 Dario Lodi Rizzini, Gabriele Penzotti.
+% 
+% vri-sprinkler-simulation is free software: you can redistribute it and/or modify
+% it under the terms of the Creative Common License.
+%
   volume_tot = sum(sum(field.water));
   height_avg = sum(sum(field.water)) / (field.x_num * field.y_num);
 end
